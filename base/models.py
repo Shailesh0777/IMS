3f659e46087687ebcde9a37c7c15bbbba487c6f0
Shailesh Paudel <paudelshail897@gmail.com>
2024-08-08 06:19:22 +0545

    changes

---
+++ base/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=300)
+    username = models.CharField(max_length=100,default='username')g
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+
+class ResourceType(models.Model):
+    name = models.CharField(max_length = 50)
+
+class Department(models.Model):
+    name = models.CharField(max_length=50)
+    floor = models.IntegerField()
+    description = models.TextField()
+
+class Resource(models.Model):
+    name = models.CharField(max_length=50)
+    type = models.ForeignKey(ResourceType, on_delete=models.SET_NULL, null = True ) #one to many relation
+    description = models.TextField()
+    quantity = models.IntegerField()
+    department = models.ManyToManyField(Department) #many to many relation
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)
+    contact_number =  models.IntegerField()
+
+class Purchase(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL,null=True)
+    resource = models.ForeignKey(Resource, on_delete=models.SET_NULL, null=True)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    description = models.TextField()
---
+++ base/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+# Create your views here.
+
+class ResourseType(ModelViewSet):
+    # queryset = 
+    # serializer_class = 
+    pass
+class Resource(ModelViewSet):
+    # queryset
+    #seralizer_class = 
+    pass
